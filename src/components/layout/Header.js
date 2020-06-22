49f73a686234a03bdff14bffcb5e879c6d48f7cd
TJCowx <34313776+TJCowx@users.noreply.github.com>
2020-06-22 17:15:27 -0400

    added material ui to the project and header

---
+++ src/components/layout/Header.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+
+export class Header extends Component {
+  render() {
+    return (
+      <>
+        <AppBar>
+          <Toolbar>
+
+          </Toolbar>
+        </AppBar>
+      </>
+    )
+  }
+}
+
+export default Header
