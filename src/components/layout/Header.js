06332de88dcc5b13a00cc8924cb2d51b35cd5c94
TJCowx <34313776+TJCowx@users.noreply.github.com>
2020-06-22 18:44:08 -0400

    Added header component and functionality

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,3 +1,7 @@
+/**
+ * Header.js - The header component for this App.
+ */
+
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
@@ -6,6 +10,7 @@ import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
 import D20Logo from "../../assets/d20Logo.svg";
 
+/** Styling for this component */
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
@@ -20,6 +25,7 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function Header() {
+  // Make sure the component is using the styles
   const classes = useStyles();
 
   return (
