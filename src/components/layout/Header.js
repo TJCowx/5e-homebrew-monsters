47261a22af54face577db76b89742af1355b70a0
TJCowx <34313776+TJCowx@users.noreply.github.com>
2020-06-22 18:41:43 -0400

    Added header component

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,19 +1,39 @@
-import React, { Component } from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import D20Logo from "../../assets/d20Logo.svg";
 
-export class Header extends Component {
-  render() {
-    return (
-      <>
-        <AppBar>
-          <Toolbar>
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    background: "#004d00",
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 
-          </Toolbar>
-        </AppBar>
-      </>
-    )
-  }
+export default function Header() {
+  const classes = useStyles();
+
+  return (
+    <>
+      <AppBar className={classes.root}>
+        <Toolbar>
+          <IconButton className={classes.menuButton} color="inherit">
+            <img src={D20Logo} alt="D20 Logo" />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            5e Homebrew Monsters
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </>
+  );
 }
-
-export default Header
