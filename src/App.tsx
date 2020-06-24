5deb5798874e048681766c07a8e790e0df7500e6
TJCowx <34313776+TJCowx@users.noreply.github.com>
2020-06-24 17:23:01 -0400

    Fixed appbar styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import Header from './components/layout/Header';
+import Monster from './components/monster/Monster';
+import { Container, makeStyles } from '@material-ui/core';
 import './App.css';
 
+const useStyles = makeStyles((theme) => ({
+  appBarSpacer: theme.mixins.toolbar,
+}));
+
 function App() {
+  const classes = useStyles();
   return (
     <div className="App">
       <Header />
+      <div className={classes.appBarSpacer} />
+      <Container maxWidth="lg">
+        <Monster />
+      </Container>
     </div>
   );
 }
