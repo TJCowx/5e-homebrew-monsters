01ab8c00902e2a228d00622f3e7e3a621525aee3
TJCowx <34313776+TJCowx@users.noreply.github.com>
2020-06-26 17:36:20 -0400

    improved the way to get theming

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Header from './components/layout/Header';
 import Monster from './components/monster/Monster';
 import { makeStyles, createMuiTheme, ThemeProvider } from '@material-ui/core';
 import './App.css';
+import { green } from '@material-ui/core/colors';
 
 const useStyles = makeStyles((theme) => ({
   appBarSpacer: theme.mixins.toolbar,
@@ -10,33 +11,9 @@ const useStyles = makeStyles((theme) => ({
 
 const theme = createMuiTheme({
   spacing: 8,
-  overrides: {
-    /** Set the styling for input fields to use the green of the header */
-    MuiOutlinedInput: {
-      root: {
-        '& $notchedOutline': {
-          borderColor: 'rgba(0, 0, 0, 0.23)',
-        },
-        '&:hover:not($disabled):not($focused):not($error) $notchedOutline': {
-          borderColor: '#004d00',
-          // Reset on touch devices, it doesn't add specificity
-          '@media (hover: none)': {
-            borderColor: 'rgba(0, 0, 0, 0.23)',
-          },
-        },
-        '&$focused $notchedOutline': {
-          borderColor: '#004d00',
-          borderWidth: 1,
-        },
-        display: 'flex',
-      },
-    },
-    MuiInputLabel: {
-      root: {
-        '&$focused': {
-          color: '#004d00',
-        },
-      },
+  palette: {
+    primary: {
+      main: '#004d00',
     },
   },
 });
