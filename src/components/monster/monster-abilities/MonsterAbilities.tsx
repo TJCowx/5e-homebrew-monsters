5324907e28e7fd6f571a4b0366184cd6af192376
TJCowx <34313776+TJCowx@users.noreply.github.com>
2020-08-18 17:18:50 -0400

    updated monster abilities component to handle the abilities better

--- src/components/monster/monster-abilities/MonsterAbilities.tsx
+++ src/components/monster/monster-abilities/MonsterAbilities.tsx
@@ -6,31 +6,26 @@ import React from 'react';
 import AddMonsterAbility from './AddMonsterAbility';
 import MonsterAbilitiesList from './MonsterAbilitiesList';
 import MonsterAbility from '../../../models/MonsterAbility';
-import PropTypes from 'prop-types';
+import PropTypes, { InferProps } from 'prop-types';
 
-export interface MonsterAbilitiesProps {
-  monsterAbilities: Array<MonsterAbility>;
-  handleChange: any;
-  classes: any;
+function MonsterAbilities({
+  monsterAbilities,
+  handleChange,
+  classes,
+}: InferProps<typeof MonsterAbilities.propTypes>) {
+  return (
+    <div style={{ width: '100%' }}>
+      <AddMonsterAbility />
+      <MonsterAbilitiesList monsterAbilities={monsterAbilities} />
+    </div>
+  );
 }
 
-class MonsterAbilities extends React.Component<MonsterAbilitiesProps> {
-  static propTypes: { [key in keyof MonsterAbilitiesProps]: any } = {
-    monsterAbilities: PropTypes.array.isRequired,
-    handleChange: PropTypes.func.isRequired,
-    classes: PropTypes.object,
-  };
-
-  private handleAddMonsterAbility = (newAbility: MonsterAbility) => {};
-
-  render() {
-    return (
-      <div>
-        <AddMonsterAbility />
-        <MonsterAbilitiesList />
-      </div>
-    );
-  }
-}
+MonsterAbilities.propTypes = {
+  monsterAbilities: PropTypes.arrayOf(PropTypes.instanceOf(MonsterAbility))
+    .isRequired,
+  handleChange: PropTypes.func,
+  classes: PropTypes.object,
+};
 
 export default MonsterAbilities;
