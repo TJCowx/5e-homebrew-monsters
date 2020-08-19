7564a57abda9e3e044a9e9f422cf31e846b260f7
TJCowx <34313776+TJCowx@users.noreply.github.com>
2020-08-19 18:32:31 -0400

    added monster action class

---
+++ src/models/MonsterAction.tsx
@@ -0,0 +1,19 @@
+import { v4 as uuidv4 } from 'uuid';
+
+export default class MonsterAction {
+  id: string;
+  name: string;
+  description: string;
+  actionType: string;
+  attackType: string;
+  toHit: string;
+  damage: string;
+
+  constructor(init?: Partial<MonsterAction>) {
+    if (init.id == null) {
+      init.id = uuidv4();
+    }
+
+    Object.assign(this, init);
+  }
+}
