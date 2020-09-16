651d52fc98e2b4f34122d1c30bab0d791289125a
TJCowx <34313776+TJCowx@users.noreply.github.com>
2020-09-16 16:46:07 -0400

    added missing comment

--- src/hooks/getSavingThrowModifier.tsx
+++ src/hooks/getSavingThrowModifier.tsx
@@ -1,7 +1,7 @@
 import { getModifier } from './getModifier';
 
 /**
- *
+ * Calculates the saving throw modifier using proficiency
  * @param savingThrow The saving throw value
  * @param profBonus The proficiency bonus
  */
