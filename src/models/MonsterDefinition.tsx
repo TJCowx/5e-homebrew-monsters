02443ebf9e52f7c7e4a705eb1dd644497f497ac1
TJCowx <34313776+TJCowx@users.noreply.github.com>
2020-10-05 19:02:46 -0400

    updated monsterdef to handle speeds better

--- src/models/MonsterDefinition.tsx
+++ src/models/MonsterDefinition.tsx
@@ -18,11 +18,11 @@ export default class MonsterDefinition {
   armourClass: string = '';
   hitPoints: string = '';
   hitDie: string = '';
-  landSpeed: number = null;
-  flySpeed: number = null;
-  burrowSpeed: number = null;
-  climbSpeed: number = null;
-  hoverSpeed: number = null;
+  landSpeed: string = '';
+  flySpeed: string = '';
+  burrowSpeed: string = '';
+  climbSpeed: string = '';
+  hoverSpeed: string = '';
   str: string = '0';
   dex: string = '0';
   con: string = '0';
